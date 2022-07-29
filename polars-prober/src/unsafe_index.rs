9336ca7ca80b553d4feea97bb6ff1c82649d5d7e
Jacob Xie <jacobbishopxy@gmail.com>
2022-07-29 17:35:01 +0800

    chore

--- polars-prober/src/unsafe_index.rs
+++ polars-prober/src/unsafe_index.rs
@@ -14,6 +14,12 @@ impl MyValueTrait for bool {
     }
 }
 
+impl MyValueTrait for i64 {
+    fn dtype(&self) -> &'static str {
+        "i64"
+    }
+}
+
 #[derive(Debug)]
 struct Null;
 
@@ -21,6 +27,42 @@ impl MyValueTrait for Null {
     fn dtype(&self) -> &'static str {
         "null"
     }
+}
+
+// This struct won't work, because for example `MyGenericValue<bool>` and `MyGenericValue<i64>` do not have the same size.
+#[allow(dead_code)]
+struct MyGenericValue<T: MyValueTrait>(T);
+
+#[test]
+fn my_generic_value_not_same_size() {
+    let v1 = MyGenericValue(true);
+    println!("{:?}", std::mem::size_of_val(&v1));
+
+    let v2 = MyGenericValue(1i64);
+    println!("{:?}", std::mem::size_of_val(&v2));
+
+    assert_ne!(std::mem::size_of_val(&v1), std::mem::size_of_val(&v2));
+}
+
+// This struct won't work, because when impl `Index`, there is no way to hold the original variable which is also UnSized.
+#[derive(Debug)]
+struct MyRefValue<'a>(&'a dyn MyValueTrait);
+
+impl<'a> AsRef<MyRefValue<'a>> for &'a dyn MyValueTrait {
+    fn as_ref(&self) -> &MyRefValue<'a> {
+        unsafe { std::mem::transmute(self) }
+    }
+}
+
+#[test]
+fn my_ref_value_as_ref() {
+    let v = true;
+
+    let dv = &v as &dyn MyValueTrait;
+
+    let dvr: &MyRefValue = dv.as_ref();
+
+    println!("{:?}", dvr);
 }
 
 #[derive(Debug)]
@@ -31,7 +73,6 @@ impl AsRef<MyValue> for Box<dyn MyValueTrait> {
         unsafe { std::mem::transmute(self) }
     }
 }
-
 #[test]
 fn my_value_as_ref() {
     let dv = Box::new(false) as Box<dyn MyValueTrait>;
@@ -39,6 +80,35 @@ fn my_value_as_ref() {
     let dvr: &MyValue = dv.as_ref();
 
     println!("{:?}", dvr);
+}
+
+impl From<bool> for MyValue {
+    fn from(v: bool) -> Self {
+        Self(Box::new(v))
+    }
+}
+
+impl From<i64> for MyValue {
+    fn from(v: i64) -> Self {
+        Self(Box::new(v))
+    }
+}
+
+impl From<Null> for MyValue {
+    fn from(v: Null) -> Self {
+        Self(Box::new(v))
+    }
+}
+
+#[test]
+fn my_value_from_x() {
+    let v1 = MyValue::from(false);
+    let v2 = MyValue::from(1i64);
+    let v3 = MyValue::from(Null);
+
+    println!("{:?}", v1);
+    println!("{:?}", v2);
+    println!("{:?}", v3);
 }
 
 struct MySeriesIndexing<'a> {
@@ -62,6 +132,7 @@ impl<'a> Index<usize> for MySeriesIndexing<'a> {
     fn index(&self, index: usize) -> &Self::Output {
         match self.data.dtype() {
             DataType::Boolean => {
+                // unpack series to `ChunkedArray`
                 let res: Box<dyn MyValueTrait> = match self.data.bool().unwrap().get(index) {
                     Some(v) => Box::new(v),
                     None => Box::new(Null),
@@ -70,9 +141,7 @@ impl<'a> Index<usize> for MySeriesIndexing<'a> {
                 let r = &self.cache as *const Box<dyn MyValueTrait>;
                 let m = r as *mut Box<dyn MyValueTrait>;
                 unsafe {
-                    // release `r`
-                    std::ptr::read(r);
-                    std::ptr::write(m, res);
+                    *m = res;
                 };
 
                 self.cache.as_ref()
@@ -84,7 +153,21 @@ impl<'a> Index<usize> for MySeriesIndexing<'a> {
             DataType::Int8 => todo!(),
             DataType::Int16 => todo!(),
             DataType::Int32 => todo!(),
-            DataType::Int64 => todo!(),
+            DataType::Int64 => {
+                // directly call `.get` method, which has a runtime casting (less efficiency)
+                let res: Box<dyn MyValueTrait> = match self.data.get(index) {
+                    AnyValue::Int64(v) => Box::new(v),
+                    _ => Box::new(Null),
+                };
+
+                let r = &self.cache as *const Box<dyn MyValueTrait>;
+                let m = r as *mut Box<dyn MyValueTrait>;
+                unsafe {
+                    *m = res;
+                };
+
+                self.cache.as_ref()
+            }
             DataType::Float32 => todo!(),
             DataType::Float64 => todo!(),
             DataType::Utf8 => todo!(),
