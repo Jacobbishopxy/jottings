68563596a25b83120c461a91d3ebe81627b52b63
jacob <jacobbishopxy@gmail.com>
2023-02-02 22:46:25 +0800

    bug: offset casting type

--- arrow-cpp/demo/lb_pqt.cpp
+++ arrow-cpp/demo/lb_pqt.cpp
@@ -20,9 +20,30 @@ struct PqtSchema
 {
   std::string symbol;
   int mi;
-  std::set<int> a;
-  std::set<int> d;
+  std::vector<int> a;
+  std::vector<int> d;
+
+  friend std::ostream& operator<<(std::ostream& os, const PqtSchema& ps);
 };
+
+std::ostream& operator<<(std::ostream& os, const PqtSchema& ps)
+{
+  os << ps.symbol << " ";
+
+  os << ps.mi << " [";
+
+  for (auto& oa : ps.a)
+    os << oa << ",";
+
+  os << "] [";
+
+  for (auto& od : ps.d)
+    os << od << ",";
+
+  os << "]";
+
+  return os;
+}
 
 const auto schema = arrow::schema({
     arrow::field("symbol", arrow::utf8()),
@@ -72,7 +93,7 @@ arrow::Result<std::shared_ptr<arrow::Table>> create_table(
       auto od = oad.get_order_D();
 
       // 两者皆无效时跳过
-      if (!oa.empty() && !od.empty())
+      if (!oa.empty() || !od.empty())
       {
         ARROW_RETURN_NOT_OK(symbol_b.Append(symbol));
         ARROW_RETURN_NOT_OK(mi_b.Append(mi));
@@ -164,6 +185,7 @@ arrow::Result<std::shared_ptr<arrow::Table>> read_pqt()
 {
   std::shared_ptr<arrow::io::ReadableFile> infile;
 
+  // 可以直接使用 auto infile 省去上方的定义
   ARROW_ASSIGN_OR_RAISE(infile, arrow::io::ReadableFile::Open(filename));
 
   std::unique_ptr<parquet::arrow::FileReader> reader;
@@ -186,14 +208,15 @@ arrow::Result<std::vector<PqtSchema>> deconstruct_table(std::shared_ptr<arrow::T
   auto mi_arr = std::static_pointer_cast<arrow::Int64Array>(table->column(1)->chunk(0));
 
   // a 字段
+  // 注意：const int64_t* 与 GetValues<int64_t>。直接使用 int 会导致指针偏移量错误，因此需要与 array 的实际类型一致！
   auto order_A_arr = std::static_pointer_cast<arrow::ListArray>(table->column(2)->chunk(0));
   auto order_A_arr_values = std::static_pointer_cast<arrow::Int64Array>(order_A_arr->values());
-  const int* oa_ptr = order_A_arr_values->data()->GetValues<int>(1);
+  const int64_t* oa_ptr = order_A_arr_values->data()->GetValues<int64_t>(1);
 
   // d 字段
   auto order_D_arr = std::static_pointer_cast<arrow::ListArray>(table->column(3)->chunk(0));
-  auto order_D_arr_values = std::static_pointer_cast<arrow::Int64Array>(order_A_arr->values());
-  const int* od_ptr = order_D_arr_values->data()->GetValues<int>(1);
+  auto order_D_arr_values = std::static_pointer_cast<arrow::Int64Array>(order_D_arr->values());
+  const int64_t* od_ptr = order_D_arr_values->data()->GetValues<int64_t>(1);
 
   std::vector<PqtSchema> res;
 
@@ -206,14 +229,15 @@ arrow::Result<std::vector<PqtSchema>> deconstruct_table(std::shared_ptr<arrow::T
     int mi = mi_arr->Value(i);
 
     // order_A
-    const int* oa_first = oa_ptr + order_A_arr->value_offset(i);
-    const int* oa_last = oa_ptr + order_A_arr->value_offset(i + 1);
-    std::set<int> oa(oa_first, oa_last);
+    // 注意：const int64_t* 与上述偏移量一致å
+    const int64_t* oa_first = oa_ptr + order_A_arr->value_offset(i);
+    const int64_t* oa_last = oa_ptr + order_A_arr->value_offset(i + 1);
+    std::vector<int> oa(oa_first, oa_last);
 
     // order_D
-    const int* od_first = od_ptr + order_D_arr->value_offset(i);
-    const int* od_last = od_ptr + order_D_arr->value_offset(i + 1);
-    std::set<int> od(od_first, od_last);
+    const int64_t* od_first = od_ptr + order_D_arr->value_offset(i);
+    const int64_t* od_last = od_ptr + order_D_arr->value_offset(i + 1);
+    std::vector<int> od(od_first, od_last);
 
     res.push_back({symbol, mi, oa, od});
   }
@@ -224,10 +248,22 @@ arrow::Result<std::vector<PqtSchema>> deconstruct_table(std::shared_ptr<arrow::T
 // mock
 arrow::Status mock_data_read()
 {
-  // TODO
+  ARROW_ASSIGN_OR_RAISE(auto table, read_pqt());
+
+  ARROW_ASSIGN_OR_RAISE(auto pqt_s_vec, deconstruct_table(table));
+
+  for (int idx{0}; auto& pqt_s : pqt_s_vec)
+  {
+    std::cout << idx << "\t" << pqt_s << std::endl;
+    idx++;
+  }
 
   return arrow::Status::OK();
 }
+
+// ================================================================================================
+// Main
+// ================================================================================================
 
 int main(int argc, char** argv)
 {
