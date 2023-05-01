d0ee939e2dc17c4365469c8145b15385044fcc4c
jacob <jacobbishopxy@gmail.com>
2023-05-01 12:53:54 +0800

    chore: rename file

---
+++ arrow-ipc/ipc-cpp/main_ipc_stream_client.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file:	main_ipc_stream_client.cpp
+ * @author:	Jacob Xie
+ * @date:	2023/05/01 12:50:30 Monday
+ * @brief:
+ **/
+
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
---
+++ arrow-ipc/ipc-cpp/main_ipc_stream_server.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file:	main_ipc_stream_server.cpp
+ * @author:	Jacob Xie
+ * @date:	2023/05/01 12:50:17 Monday
+ * @brief:
+ **/
+
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
