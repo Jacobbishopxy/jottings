acb0512548add13da441d83a05de7b7b79463713
jacob <jacobbishopxy@gmail.com>
2023-05-21 23:52:24 +0800

    mnt: add streaming `stdout`

--- pycmd/scripts/dev.py
+++ pycmd/scripts/dev.py
@@ -3,8 +3,16 @@
 # @date:	2023/04/07 18:14:16 Friday
 # @brief:
 
-import pandas as pd
+import time
 
-foo = pd.read_csv("./dev.csv")
+with open("./dev.csv") as f:
+    count = 0
+    while 1:
+        line = f.readline()
 
-print(foo)
+        if not line:
+            break
+        print(f"Line{count}: {line.strip()}")
+        count += 1
+
+        time.sleep(1)
--- pycmd/src/main.rs
+++ pycmd/src/main.rs
@@ -2,25 +2,82 @@
 //! author: Jacob Xie
 //! date: 2023/04/07 18:04:35 Friday
 //! brief:
+//!
+//! check <https://doc.rust-lang.org/std/process/struct.Command.html> for more.
 
-use std::process::Command;
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
 
-fn main() -> std::io::Result<()> {
-    let python_cmd = format!("{}/python", "/opt/homebrew/anaconda3/envs/py310/bin");
-
-    // bash -c "cd ~/Code/jotting/pycmd/scripts/ && /opt/homebrew/anaconda3/envs/py310/bin/python dev.py"
-
+#[allow(dead_code)]
+fn exec_and_print_at_once(py_cmd: &str) -> std::io::Result<()> {
     let output = Command::new("bash")
         .arg("-c")
         .arg(format!(
             "cd ~/Code/jotting/pycmd/scripts && {} dev.py",
-            python_cmd
+            py_cmd
         ))
         .output()?;
 
     println!("Status code: {}", output.status);
     println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
     println!("Error:\n{}", String::from_utf8_lossy(&output.stderr));
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn exec_and_print_with_buf(py_cmd: &str) -> std::io::Result<()> {
+    let mut cmd = Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "cd ~/Code/jotting/pycmd/scripts && {} dev.py",
+            py_cmd
+        ))
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let stdout = cmd.stdout.as_mut().unwrap();
+    let stdout_reader = BufReader::new(stdout);
+    let stdout_lines = stdout_reader.lines();
+
+    for line in stdout_lines {
+        println!("READ: {:?}", line);
+    }
+
+    cmd.wait()?;
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn exec_and_print_inherit(py_cmd: &str) -> std::io::Result<()> {
+    let mut cmd = Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "cd ~/Code/jotting/pycmd/scripts && {} dev.py",
+            py_cmd
+        ))
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()?;
+
+    // It's streaming here
+    let status = cmd.wait()?;
+    println!("Exited with status {:?}", status);
+
+    Ok(())
+}
+
+// TODO: more flexible python calling: `conda activate [xxx]; python [py_script]`
+
+fn main() -> std::io::Result<()> {
+    let py_cmd = format!("{}/python", "/opt/homebrew/anaconda3/envs/py310/bin");
+
+    // bash -c "cd ~/Code/jotting/pycmd/scripts/ && /opt/homebrew/anaconda3/envs/py310/bin/python dev.py"
+
+    // exec_and_print_at_once(&py_cmd)?;
+    // exec_and_print_with_buf(&py_cmd)?;
+    exec_and_print_inherit(&py_cmd)?;
 
     Ok(())
 }
