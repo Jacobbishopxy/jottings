f8da5cddfb98cd89bb2341caa2e276ab7ccdfd70
jacob <jacobbishopxy@gmail.com>
2023-06-03 00:07:13 +0800

    :package:

--- std-traits/src/lib.rs
+++ std-traits/src/lib.rs
@@ -1,6 +1,7 @@
 //! std-traits playground
 
 #![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
 
 mod case_asmut;
 mod commands;
@@ -9,6 +10,6 @@ mod tree_node_traverse;
 mod gat_async_trait;
 
 mod gat_fp;
-mod iter_filter_final;
 mod iter_filter_dumb;
+mod iter_filter_final;
 mod iter_filter_improved;
